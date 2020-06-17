ba192e50ae3db298545012209cd2011ac672e82a
dothinhung123 <youwillbesucessful@gmail.com>
2020-06-17 07:39:26 +0700

    Add all file

---
+++ Assets/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    Text text;
+    
+    float theTime;
+    public float speed = 1;
+    private EGameState _gameState;
+    public EPlayer CurrentPlayer { get; private set; }
+
+    public EGameState GameState
+    {
+        get { return _gameState; }
+        set { _gameState = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text = GetComponent<Text>();
+ 
+        theTime += Time.deltaTime * speed;
+            string hours = Mathf.Floor((theTime % 216000) / 3600).ToString("00");
+            string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+            string seconds = (theTime % 60).ToString("00");
+            text.text = hours + ":" + minutes + ":" + seconds;
+           
+        
+
+        
+        }
+    
+
+   
+}
---
+++ Assets/_Scripts/CTLs/BaseGameCTL.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BaseGameCTL : MonoBehaviour
+{
+    public Text txt_current_player;
+ 
+
+    public static BaseGameCTL Current;
+    GameObject button;
+
+    public const float TIME_TO_THINK = 5;
+    public Text txt_time;
+    private float startTime;
+    private bool timeOutIsCalled = false;
+
+    private EGameState _gameState;
+    public EPlayer CurrentPlayer { get; private set; }
+
+    public EGameState GameState
+    {
+        get { return _gameState; }
+        set { _gameState = value; }
+    }
+
+
+    void Awake()
+    {
+        Current = this;
+        CurrentPlayer = EPlayer.WHITE;
+        GameState = EGameState.PLAYING;
+       
+    }
+
+    void Start()
+    {
+    
+        // StartCoroutine(UpdateTime());
+    }
+
+    void Update()
+    {
+       
+       
+    }
+
+   
+
+
+    /// <summary>
+    /// Chuyển lượt chơi
+    /// </summary>
+    public void SwitchTurn()
+    {
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (ChessBoard.Current.Cells[i][j].State != ECellState.NORMAL)
+                    ChessBoard.Current.Cells[i][j].SetCellState(ECellState.NORMAL);
+            }
+        }
+
+        timeOutIsCalled = false;
+      
+        CurrentPlayer = CurrentPlayer == EPlayer.WHITE ? EPlayer.BLACK : EPlayer.WHITE;
+
+        switch (CurrentPlayer)
+        {
+            case EPlayer.BLACK:
+                txt_current_player.text = "BLACK";
+                txt_current_player.color = Color.black;
+                break;
+            case EPlayer.WHITE:
+                txt_current_player.text = "WHITE";
+                txt_current_player.color = Color.white;
+                break;
+            default:
+                break;
+        }
+    
+    }
+
+    /// <summary>
+    /// Kiểm tra trạng thái của bàn cờ, xem đã kết thúc hay chưa
+    /// </summary>
+    public EGameState CheckGameState()
+    {
+
+        return EGameState.PLAYING;
+    }
+
+    public void GameOver(EPlayer winPlayer)
+    {
+        GameState = EGameState.GAME_OVER;
+        Debug.Log("WinPlayer : " + winPlayer);
+        StartCoroutine(x());
+       
+    }
+    IEnumerator x()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("TwoPlayersScene");
+
+    }
+
+
+}
